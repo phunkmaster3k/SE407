24c95023b456a35c75b241c94b99455336f4b8bc
Nathan Livernois <nslivernois@email.neit.edu>
2018-01-08 20:40:23 -0500

    Week 1 in class done, lab created

--- MyWebsite/MyWebsite/App_Start/RouteConfig.cs
+++ MyWebsite/MyWebsite/App_Start/RouteConfig.cs
@@ -5,17 +5,13 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Lab1
+namespace MyWebsite
 {
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(" Contact", "Contact", new { controller = "Home", action = "Contact" });
-
-            routes.MapRoute(" About", "About", new { controller = "Home", action = "About" });
 
             routes.MapRoute(
                 name: "Default",
