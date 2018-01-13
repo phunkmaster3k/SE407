b981920dab8602475ed61b3c73d22b9c61a3e7ea
Nathan Livernois <nslivernois@email.neit.edu>
2018-01-13 11:34:01 -0500

    rss test good

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.ServiceModel.Syndication;
 
 namespace ConsoleApplication1
 {
@@ -10,7 +12,26 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //test
+            string url = "https://robertsspaceindustries.com/comm-link/rss";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+            foreach (SyndicationItem item in feed.Items)
+            {
+                Console.WriteLine( item.Title.Text );
+                Console.WriteLine( item.Summary.Text);
+                Console.WriteLine(item.Links[0].Uri);
+                if ( item.Categories.Count > 0)
+                {
+                    Console.WriteLine(item.Categories[0].Name);
+                }
+
+                
+
+
+            }
+            Console.ReadLine();
         }
     }
 }
