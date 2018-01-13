361fd8ffedec9cb5a46716f6b5482a4e56254e8d
Nathan Livernois <nslivernois@email.neit.edu>
2018-01-13 12:55:57 -0500

    added rss cards

--- SCFOWebsite/SCFOWebsite/Controllers/HomeController.cs
+++ SCFOWebsite/SCFOWebsite/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.ServiceModel.Syndication;
 
 namespace SCFOWebsite.Controllers
 {
@@ -10,6 +12,34 @@ namespace SCFOWebsite.Controllers
     {
         public ActionResult Index()
         {
+            string url = "https://robertsspaceindustries.com/comm-link/rss";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+
+            List<SyndicationItem> items = feed.Items.ToList();
+
+            ViewBag.RSSList = items;
+
+            //foreach (SyndicationItem item in feed.Items)
+            //{
+
+                /*Console.WriteLine(item.Title.Text);
+                Console.WriteLine(item.Summary.Text);
+                Console.WriteLine(item.Links[0].Uri);
+                if (item.Categories.Count > 0)
+                {
+                    Console.WriteLine(item.Categories[0].Name);
+                }*/
+
+
+
+
+            //}
+
+
+
+
             return View();
         }
 
