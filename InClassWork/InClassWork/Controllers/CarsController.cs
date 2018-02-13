9bb98b8335544dd0bb05ffd80ccc1d5ca7c1d7c2
Nathan Livernois <nslivernois@email.neit.edu>
2018-02-12 19:38:35 -0500

    clas week 6 done

--- InClassWork/InClassWork/Controllers/CarsController.cs
+++ InClassWork/InClassWork/Controllers/CarsController.cs
@@ -10,6 +10,7 @@ namespace InClassWork.Controllers
 {
     public class CarsController : Controller
     {
+        private CarFactory Db = new CarFactory();
         // GET: Cars
         public ActionResult CarsList()
         {
@@ -17,6 +18,30 @@ namespace InClassWork.Controllers
             var viewModel = new CarListViewModel(factory.Cars);
 
             return View(viewModel);
+        }
+
+        public ActionResult ListOfCars(string searchStr)
+        {
+            var factory = new CarFactory();
+            IQueryable<Car> cars = factory.Cars.OrderBy(p => p.Model);
+
+            if ( searchStr != null )
+            {
+                cars = cars.Where(p => p.Model.Contains(searchStr));
+            }
+
+            var carsList = cars.Take(5).ToList();
+
+            //var cars = new CarFactory().Cars.ToList();
+            
+            return View(carsList);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var factory = new CarFactory();
+            Car found = factory.Cars.Where(p => p.Car_ID == id).FirstOrDefault();
+            return View(found);
         }
     }
 }
